cf5de79860ba9d8c9a03f4a05eedacf58c474647
Guleaev <gvladisl@techteam.com>
2018-07-29 16:31:44 +0300

    Changed package public api

--- dynamic-autocomplete/public_api.ts
+++ dynamic-autocomplete/public_api.ts
@@ -1,4 +1,3 @@
-export { AbstractValueAccessor } from './src/app/autocomplete/abstract-value-accessor';
 export { AutocompleteService } from './src/app/autocomplete/autocomplete.service';
 
 export * from './src/app/autocomplete/autocomplete.module';
